9afa39b64d2bbb62450a8c9670fe2ffbdd6c511f
Tianle Chen <tianlechen@gmail.com>
2023-04-20 20:11:13 +0000

    rework test data path

--- tests/parsed_test.py
+++ tests/parsed_test.py
@@ -6,6 +6,9 @@ from dinesafe.parsed import (
 )
 from dinesafe.data import DataSource
 
+NEW_ESTABLISHMENTS = get_parsed_establishments("tests/test_data/dinesafe/new.xml")
+OLD_ESTABLISHMENTS = get_parsed_establishments("tests/test_data/dinesafe/old.xml")
+
 
 def test_data_source(data_source: DataSource):
     establishments = get_parsed_establishments(data_source.latest_path)
@@ -16,16 +19,12 @@ def test_data_source(data_source: DataSource):
 
 
 def test_get_new_establishments():
-    new = get_parsed_establishments("tests/data/dinesafe/new.xml")
-    old = get_parsed_establishments("tests/data/dinesafe/old.xml")
-    actual = get_new_establishments(new=new, old=old)
+    actual = get_new_establishments(new=NEW_ESTABLISHMENTS, old=OLD_ESTABLISHMENTS)
     assert len(actual) == 1
 
 
 def test_get_new_inspections():
-    new = get_parsed_establishments("tests/data/dinesafe/new.xml")
-    old = get_parsed_establishments("tests/data/dinesafe/old.xml")
-    actual = get_new_inspections(new=new, old=old)
+    actual = get_new_inspections(new=NEW_ESTABLISHMENTS, old=OLD_ESTABLISHMENTS)
     assert len(actual) == 1
     for new_inspections_by_date in actual.values():
         assert len(new_inspections_by_date) == 1
