26c3ae3b5c9b11c48938e2b8d64441714d6e1cb3
Tianle Chen <tianlechen@gmail.com>
2023-04-20 19:21:18 +0000

    update tests

--- tests/parsed_test.py
+++ tests/parsed_test.py
@@ -5,4 +5,6 @@ from dinesafe.data import DataSource
 def test_data_source(data_source: DataSource):
     establishments = get_parsed_establishments(data_source.latest_path)
     assert len(establishments) > 1
-    assert isinstance(establishments[0], Establishment)
+    for establishment in establishments.values():
+        assert isinstance(establishment, Establishment)
+        break
