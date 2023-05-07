7b658329bb0ff9b0a97fd5229041fb2c3439d83e
Tianle Chen <tianlechen@gmail.com>
2023-05-07 00:59:06 +0000

    implement some io

--- dinesafe/data/db/io.py
+++ dinesafe/data/db/io.py
@@ -1,24 +1,53 @@
 from datetime import date
 from typing import Dict
-
+from sqlalchemy import Connection, Row
 from dinesafe.data.db.types import Establishment, Inspection
+from typing import List
+from sqlalchemy import text
+import pandas as pd
+import logging
+from dataclasses import asdict
+
+logger = logging.getLogger(__name__)
 
 
-def add_new_establishment_if_not_exists(establishment: Establishment):
+def parse_establishment_row(row: Row) -> Establishment:
+    return Establishment(**row._asdict())
+
+
+def parse_inspection_row(row: Row) -> Inspection:
+    return Inspection(**row._asdict())
+
+
+def add_new_establishment_if_not_exists(conn: Connection, establishment: Establishment):
+    result = conn.execute(
+        text(
+            f"SELECT * FROM establishment WHERE establishment_id = {establishment.establishment_id}"
+        )
+    )
+    if len(list(result)) == 0:
+        print(
+            f"Adding establishment id: {establishment.establishment_id} since none was found"
+        )
+        pd.DataFrame(data=[asdict(establishment)]).to_sql(
+            name="establishment", con=conn, if_exists="append", index=False
+        )
+        return True
+    return False
+
+
+def add_new_inspection_if_not_exists(conn: Connection, inspection: Inspection):
     pass
 
 
-def add_new_inspection_if_not_exists(inspection: Inspection):
+def get_establishments(conn: Connection) -> Dict[str, Establishment]:
+    result = conn.execute(text("SELECT * FROM establishment"))
+    return [parse_establishment_row(row=row) for row in result]
+
+
+def get_inspections(conn: Connection, establishment: Establishment):
     pass
 
 
-def get_establishments() -> Dict[str, Establishment]:
-    pass
-
-
-def get_inspections(establishment: Establishment):
-    pass
-
-
-def get_new_inspections_since(dt: date):
+def get_new_inspections_since(conn: Connection, dt: date) -> List[Inspection]:
     pass
---
+++ tests/data/db/io_test.py
@@ -0,0 +1,27 @@
+from dinesafe.data.db.engine import get_inmemory_engine
+from dinesafe.data.db.io import get_establishments, add_new_establishment_if_not_exists
+from sqlalchemy import text
+from dinesafe.data.dinesafeto.convert import (
+    convert_dinesafeto_establishment,
+    convert_dinesafeto_inspection,
+)
+from dinesafe.data.dinesafeto.parsed import get_parsed_establishments
+
+
+def test_add_new_establishment_if_not_exists():
+    engine = get_inmemory_engine()
+    with engine.connect() as conn:
+        with open("dinesafe/data/db/sql/create_establishment.sql") as f:
+            conn.execute(text(f.read()))
+        print(get_establishments(conn=conn))
+
+        dinesafeto_establishments = get_parsed_establishments(
+            path_to_xml="tests/test_data/dinesafe/old.xml"
+        )
+        for dinesafeto_establishment in dinesafeto_establishments.values():
+            establishment = convert_dinesafeto_establishment(
+                dinesafeto_establishment=dinesafeto_establishment
+            )
+            add_new_establishment_if_not_exists(conn=conn, establishment=establishment)
+            break
+        print(get_establishments(conn=conn))
