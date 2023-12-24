e7249766f179a8b8ce2cdccf6562919daf6fabb0
Tianle Chen <tianlechen@gmail.com>
2023-12-24 14:21:03 +0000

    improve data density

--- views/search_results.py
+++ views/search_results.py
@@ -1,35 +1,47 @@
-import json
 from datetime import datetime
-from typing import List, Tuple
+from typing import List
 
 import streamlit as st
 
 from dinesafe.data.types import Establishment, Inspection
 from views.yelp_ratings import get_formatted_yelp_business_rating
 
-establishment_md_str = """
-#### {rank}. **{name}**
-*Address: {address} ({lat:.4f}, {lon:.4f})*
-"""
-
-latest_inspection_md_str = """
-<p style="color:{status_color}">
-    Latest inspection on {inspection_dt_str}: {status}
-</p>
-"""
-
 
 def get_dt_str(dt: datetime) -> str:
     return dt.strftime("%Y-%m-%d")
 
 
+infraction_md_str = """
+#### {date_str}
+- Severity: {severity}
+- Deficiency: {deficiency}
+"""
+
+
+def show_latest_inspection_results(inspection: Inspection):
+    with st.expander(
+        label=f'Details of inspection on {get_dt_str(inspection.date)} {"✅" if inspection.is_pass else "❌"}'
+    ):
+        for infraction in inspection.infractions:
+            st.markdown(
+                infraction_md_str.format(
+                    date_str=get_dt_str(infraction.conviction_date)
+                    if infraction.conviction_date is not None
+                    else "???",
+                    severity=infraction.severity,
+                    deficiency=infraction.deficiency,
+                )
+            )
+
+
+establishment_md_str = """
+### {rank}. **{name}**
+*Address: {address} ({lat:.4f}, {lon:.4f})*
+"""
+
+
 def search_results(most_relevant: List[Establishment]):
     for i, establishment in enumerate(most_relevant):
-        inspections = []
-        for v in establishment.inspections.values():
-            inspections += v
-        inspections.sort(key=lambda x: x.date, reverse=True)
-
         st.markdown(
             establishment_md_str.format(
                 rank=i + 1,
@@ -39,24 +51,22 @@ def search_results(most_relevant: List[Establishment]):
                 lon=establishment.longitude,
             )
         )
-        yelp_rating_md_str = get_formatted_yelp_business_rating(
-            establishment=establishment
-        )
-        st.markdown(yelp_rating_md_str, unsafe_allow_html=True)
-
-        # print the latest inspection result
+        inspections = establishment.inspections_latest_first
         if len(inspections) > 0:
-            latest_inspection = inspections[0]
+            show_latest_inspection_results(inspection=inspections[0])
+            pass_proportion = sum(
+                [1 if inspection.is_pass else 0 for inspection in inspections]
+            ) / len(inspections)
             st.markdown(
-                latest_inspection_md_str.format(
-                    status_color="Green" if latest_inspection.is_pass else "Red",
-                    status="Pass" if latest_inspection.is_pass else "Fail",
-                    inspection_dt_str=get_dt_str(latest_inspection.date),
-                ),
-                unsafe_allow_html=True,
+                f"Past {len(inspections)} inspections "
+                + f"({100*pass_proportion:.0f}% pass rate). "
+                + "Most recent first: "
+                + "".join(
+                    ["✅" if inspection.is_pass else "❌" for inspection in inspections]
+                )
             )
-        # older inspections if any including latest
-        for inspection in inspections:
-            expander_title = f"Inspection on {get_dt_str(inspection.date)}: "
-            expander_title += "✅" if inspection.is_pass else "❌"
+        st.markdown(
+            get_formatted_yelp_business_rating(establishment=establishment),
+            unsafe_allow_html=True,
+        )
         st.markdown("----")
