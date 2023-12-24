d245c6020d66eabdfd58d15c66068cc01804e0d7
Tianle Chen <tianlechen@gmail.com>
2023-12-24 02:04:09 +0000

    fix tests

--- tests/yelp_test.py
+++ tests/yelp_test.py
@@ -1,17 +1,11 @@
-from typing import Dict
-
-from dinesafe.data.dinesafeto.convert import convert_dinesafeto_establishment
-from dinesafe.data.dinesafeto.types import Establishment
+from dinesafe.data.parsed import get_establishments_from_xml
 from dinesafe.yelp import get_yelp_biz_search_top_result
 
 
-def test_get_yelp_biz_search_result(
-    old_parsed_dinesafetoestablishments: Dict[str, Establishment]
-):
-    for dinesafeto_establishment in old_parsed_dinesafetoestablishments.values():
-        establishment = convert_dinesafeto_establishment(
-            dsto_estab=dinesafeto_establishment
-        )
-        yelp_biz_result = get_yelp_biz_search_top_result(establishment=establishment)
+def test_get_yelp_biz_search_result():
+    for e in get_establishments_from_xml(
+        "tests/test_data/dinesafe/1000.01.xml"
+    ).values():
+        yelp_biz_result = get_yelp_biz_search_top_result(establishment=e)
         assert yelp_biz_result is not None
         break
